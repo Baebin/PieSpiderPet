cbb9ef0aab5f5e2dbcfe7a8c323f715c139fff6b
Baebin <63220297+Baebin@users.noreply.github.com>
2024-03-22 12:28:59 +0900

    Init Location Edited

--- lib/spider_page.dart
+++ lib/spider_page.dart
@@ -5,7 +5,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pie_spider_pet/entity/location.dart';
 import 'package:pie_spider_pet/entity/spider.dart';
 import 'package:pie_spider_pet/utils/image_path.dart';
 import 'package:screen_retriever/screen_retriever.dart';
@@ -23,16 +22,20 @@ class SpiderPageState extends ConsumerState<SpiderPage> {
 
   @override
   void initState() {
-    screenRetriever.getPrimaryDisplay().then((display) {
+    screenRetriever.getPrimaryDisplay().then((display) async {
       Size size = display.size;
       spider.window.width = size.width;
       spider.window.height = size.height;
       print('size: ${size.width} ${size.height}');
 
+      spider.location.x = spider.window.width/2;
+      spider.location.y = spider.window.height/2;
+
+      await windowManager.setPosition(
+        Offset(spider.location.x, spider.location.y),
+      );
       Timer.periodic(const Duration(milliseconds: 3000), (timer) async {
         Display display = await screenRetriever.getPrimaryDisplay();
-        print('${display.size.width}');
-
         double range = Random().nextDouble() * 500.0 + 50.0;
         spider.moveRadius(
           range: range,
@@ -43,8 +46,10 @@ class SpiderPageState extends ConsumerState<SpiderPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      ImagePath.spider,
+    return Container(
+      child: Image.asset(
+        ImagePath.spider,
+      ),
     );
   }
 }
